68950358c683e0ae58e60e86f281e49ff91a3df2
Madhankumar Balasubramani <39543@hexaware.com>
2019-09-02 17:24:39 +0530

    First time commit

---
+++ src/main/java/cucumber/samplegitproject/App.java
@@ -0,0 +1,13 @@
+package cucumber.samplegitproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cucumber/samplegitproject/samplejavaprogram.java
@@ -0,0 +1,8 @@
+package cucumber.samplegitproject;
+
+public class samplejavaprogram {
+	public static void main(String[] args) {
+		System.out.println("Git Sample Project");
+	}
+
+}
