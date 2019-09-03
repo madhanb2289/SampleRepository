a0285547953256a8b7e7229190424b5e861666ea
Madhankumar Balasubramani <39543@hexaware.com>
2019-09-03 07:25:32 +0530

    check

--- src/test/java/cucumber/samplegitproject/sampleLoginModule.java
+++ src/test/java/cucumber/samplegitproject/sampleLoginModule.java
@@ -3,8 +3,9 @@ package cucumber.samplegitproject;
 public class sampleLoginModule {
 	
 	public static void main(String[] args) {
-		System.out.print("Test check sample code");
-		System.out.println("check topics");
+		System.out.print("Test check sample code to get conflict"); 
+		System.out.println("check diff level topics");
+		System.out.println("done");
 	}
 
 }
