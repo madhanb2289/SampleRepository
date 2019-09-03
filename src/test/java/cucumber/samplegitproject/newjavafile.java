0ec21daaf7d4c7f283ce745f887345a51ccfd88a
Madhankumar Balasubramani <39543@hexaware.com>
2019-09-03 17:47:49 +0530

    added java file

---
+++ src/test/java/cucumber/samplegitproject/newjavafile.java
@@ -0,0 +1,8 @@
+package cucumber.samplegitproject;
+
+public class newjavafile {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
---
+++ src/test/java/cucumber/samplegitproject/sam.java
@@ -0,0 +1,8 @@
+package cucumber.samplegitproject;
+
+public class sam {
+	public static void main(String[] args) {
+		System.out.println("welcome");
+	}
+
+}
